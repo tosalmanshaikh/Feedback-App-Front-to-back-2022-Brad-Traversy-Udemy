8846eb3ca6d6545dd4c4e91fa9ca7f06ba18b7d7
tosalmanshaikh <tosalmanshaikh@gmail.com>
2022-04-04 12:35:26 +0200

    minor update with some basics

--- src/App.js
+++ src/App.js
@@ -1,102 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
-import Header from './components/Header';
-import FeedbackList from './components/FeedbackList';
-import FeedbackData from './components/data/FeedbackData';
+import React from "react";
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./components/data/FeedbackData";
 
 function App() {
+   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const [feedback, setFeedbackx] = useState(FeedbackData);
-
-  return (
-    <>
-    <Header />
-    <div className='container'>
-      <FeedbackList/>
-      <h1>git hub test-03</h1>
-    </div>
-    </>
-  )
+   return (
+      <>
+         <Header />
+         <div className="container">
+            <FeedbackList feedback={feedback} />
+         </div>
+      </>
+   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// -> Please ignore everything below, Thank you! 
-
-// function App() {
-//   const title = "Blog Post";
-//   const body = "This is my blog post";
-//   const comments = [
-//     { id: 1, text: "Comment one" },
-//     { id: 2, text: "Comment two" },
-//     { id: 3, text: "Comment three" },
-//   ];
-
-//   const loading = false;
-//   const showComments = true;
-
-//   if (loading) return <h1>Loading...</h1>
-
-
-//   const commmentBlock = (        
-//   <div className="comments">
-//   <h3>Comments ({comments.length})</h3>
-
-//   <ul>
-//     {comments.map((comment, index)=> (
-//       //never forget to add the key to the child element when you are mapping!  
-//       <li key={index}>
-//         {comment.text}
-//       </li>
-//     ))}
-//   </ul>
-// </div>)
-
-//   return (
-//     //always one parent element in this case <></>
-//     <>
-//       <div className="container">
-//         <h1>{title.toUpperCase()}</h1>
-//         <p>{body}</p>
-
-//         {showComments && commmentBlock}
-
-//       </div>
-
-//       {/* {Math.random() * (5 + 5)} */}
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-// if we were to write the same thing as above but just using simple Javascript, you can imagine that this going to get really messy without JSX
-//this would display: "My App"
-
-// import React from 'react'
-
-// const App = () => {
-//   return (React.createElement('div', {className: 'container'},React.createElement('h1',{},'My app'))
-
-//   )
-// }
-
-// export default App;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import FeedbackItem from './FeedbackItem'
 
-function FeedbackList() {
+function FeedbackList({feedback}) {
+
+  if(!feedback || feedback.length === 0){
+    return <p>No feedback yet</p>
+  }
+
   return (
-    <div>
-      List
+    <div className='feedback-list'>
+      {feedback.map ((item)=> (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
     </div>
   )
 }
