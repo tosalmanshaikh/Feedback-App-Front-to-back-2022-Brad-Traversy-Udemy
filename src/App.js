1e87c591f19d072991d3b8db39e2943cfe8c27a3
tosalmanshaikh <tosalmanshaikh@gmail.com>
2022-04-13 13:37:05 +0200

    Feedback app done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./components/data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./components/pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
 
 function App() {
    const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,15 +24,32 @@ function App() {
    };
 
    return (
-      <>
+      <Router>
          <Header />
 
          <div className="container">
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            <Routes>
+               <Route
+                  exact
+                  path="/"
+                  element={
+                     <>
+                        <FeedbackForm handleAdd={addFeedback} />
+                        <FeedbackStats feedback={feedback} />
+                        <FeedbackList
+                           feedback={feedback}
+                           handleDelete={deleteFeedback}
+                        />
+                     </>
+                  }
+               ></Route>
+
+               <Route path="/about" element={<AboutPage />} />
+            </Routes>
+
+            <AboutIconLink />
          </div>
-      </>
+      </Router>
    );
 }
 
